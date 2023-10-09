ada14fd3bfb601e54b2c598a4d78ae0a63fa6b8f
despxndency <140993803+despxndency@users.noreply.github.com>
2023-10-09 14:54:16 +1300

    Update servo2.ino

--- servo2.ino
+++ servo2.ino
@@ -1,32 +1,31 @@
 
 /*
-    1.  If an object is detected, turn the 
-      servo motor 10 degrees
+    1.  If an object is detected, stop the servo
     2.  If there is no object detected, 
       turn the servo motor 150 degrees
   */
 
   #include <Servo.h>
-  Servo Serv;
+  Servo Serv2;
 
   int pinIR=5;
   int pinServo=3;
   int val=0;
 
   void setup(){
-    Serv.attach(pinServo);
+    Serv2.attach(pinServo);
   }
 
   void loop(){
     val = digitalRead(pinIR);
   
     if (val ==0){
-      Serv.write(150);
+      Serv2.write(150);
       delay(100);
     }
     else
     {
-      Serv.write(10);
-      delay(100);
+      Serv2.write(0);
+      delay(1000000);
     }
   }
