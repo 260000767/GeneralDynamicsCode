a5c746cb369f45a8e901674baf7c192a81da5d91
despxndency <140993803+despxndency@users.noreply.github.com>
2023-10-09 14:51:46 +1300

    Update ultrasonic sensor2.ino

--- ultrasonic sensor2.ino
+++ ultrasonic sensor2.ino
@@ -10,7 +10,7 @@ const int trigPin = 10;
 const int echoPin = 11;
 // defines variables
 long duration;
-int distance;
+int distance2;
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
@@ -27,8 +27,8 @@ void loop() {
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2;
+  distance2 = duration * 0.034 / 2;
   // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  Serial.print("Distance2: ");
+  Serial.println(distance2);
 }
