fe9497bcbf3b5bf0d596c12405abe18a0c0e3402
despxndency <140993803+despxndency@users.noreply.github.com>
2023-10-09 14:40:40 +1300

    Update servo.ino

---
+++ servo.ino
@@ -0,0 +1,32 @@
+
+/*
+    1.  If an object is detected, turn the 
+      servo motor 10 degrees
+    2.  If there is no object detected, 
+      turn the servo motor 150 degrees
+  */
+
+  #include <Servo.h>
+  Servo Serv;
+
+  int pinIR=5;
+  int pinServo=3;
+  int val=0;
+
+  void setup(){
+    Serv.attach(pinServo);
+  }
+
+  void loop(){
+    val = digitalRead(pinIR);
+  
+    if (val ==0){
+      Serv.write(150);
+      delay(100);
+    }
+    else
+    {
+      Serv.write(10);
+      delay(100);
+    }
+  }
